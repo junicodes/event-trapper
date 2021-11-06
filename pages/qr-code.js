15a6a4992b9db219f4732813994070f9b27e1b68
Obi Okechukwu Godwin <junicodesdev@gmail.com>
2021-11-06 07:45:07 +0100

    fix

---
+++ pages/qr-code.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import QRCode from "react-qr-code";
+
+
+export default function QrCodePage() {
+    
+    return (
+        <main>
+            <QRCode value="family" />
+        </main>
+    )
+}
---
+++ pages/qr-reader.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react'
+import dynamic from "next/dynamic";
+
+const QrReader = dynamic(() => import('react-qr-reader'),{ ssr: false })
+
+class QrReaderPage extends Component {
+  state = {
+    result: 'No result'
+  }
+ 
+  handleScan = data => {
+    if (data) {
+      this.setState({
+        result: data
+      })
+    }
+  }
+  handleError = err => {
+    console.error(err)
+  }
+  render() {
+    return (
+      <div className="container">
+        <QrReader
+          delay={300}
+          onError={this.handleError}
+          onScan={this.handleScan}
+          style={{ width: '100%' }}
+        />
+        <p>{this.state.result}</p>
+
+        <style jsx>{`
+          .container {
+            width: 30%;
+            heigth: 30px;
+            margin: auto;
+            margin-top: 100px;
+          }
+        `}</style>
+      </div>
+    )
+  }
+}
+
+export default QrReaderPage;
